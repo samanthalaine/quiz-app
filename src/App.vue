2de2c0edac52708071c7d9d58157e6f9dbf1ffed
samanthalaine <samantha@voicethread.net>
2022-04-07 17:07:10 -0400

    update template

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,6 @@
 import { ref, computed } from "vue";
 
 const questions = ref([
-
   {
     question: "Which of the following lets you create a variable?",
     answer: 3,
@@ -25,46 +24,71 @@ const questions = ref([
     options: ["toUppperCase()", "toCapital()", "toLocaleStringUpperCase()"],
     selected: null,
   },
-
 ]);
 
-const quizCompleted = ref(false)
-const currentQuestion = ref(0)
+const quizCompleted = ref(false);
+const currentQuestion = ref(0);
 
-const score = computed(()=>{
-  let value = 0
-  questions.value.map(quest => {
-    if (quest.selected === quest.answer){
-      value++
+const score = computed(() => {
+  let value = 0;
+  questions.value.map((quest) => {
+    if (quest.selected === quest.answer) {
+      value++;
     }
-  })
-  return value
-})
+  });
+  return value;
+});
 
-const getCurrentQuestion = computed(()=>{
-  let question = questions.value[currentQuestion.value]
-  question.index = currentQuestion.value
-  return question
-})
+const getCurrentQuestion = computed(() => {
+  let question = questions.value[currentQuestion.value];
+  question.index = currentQuestion.value;
+  return question;
+});
 
-const setAnswer = e =>{
-  questions.value[currentQuestion.value].selected = e.target.value
-  e.target.value = null
-}
+const setAnswer = (e) => {
+  questions.value[currentQuestion.value].selected = e.target.value;
+  e.target.value = null;
+};
 
 const nextQuestion = () => {
-	if (currentQuestion.value < questions.value.length - 1) {
-		currentQuestion.value++
-		return
-	}
-	
-	quizCompleted.value = true
-}
+  if (currentQuestion.value < questions.value.length - 1) {
+    currentQuestion.value++;
+    return;
+  }
 
+  quizCompleted.value = true;
+};
 </script>
 
 <template>
-  <h1 class="text-blue-500">hello world</h1>
+  <main>
+    <h1 class="text-blue-500">The Quiz</h1>
+    <section class="quiz">
+      <div>
+        <span>{{ getCurrentQuestion.question }}</span>
+        <span>Score{{ score }}/{{ questions.length }}</span>
+        <div>
+          <label 
+					v-for="(option, index) in getCurrentQuestion.options" 
+					:for="'option' + index" 
+					:class="`option ${
+						getCurrentQuestion.selected == index 
+							? index == getCurrentQuestion.answer 
+								? 'correct' 
+								: 'wrong'
+							: ''
+					} ${
+						getCurrentQuestion.selected != null &&
+						index != getCurrentQuestion.selected
+							? 'disabled'
+							: ''
+					}`">
+            <input type="radio" :name="getCurrentQuestion.index" :value="index"> 
+          </label>
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <style>
